713561870c771996bdf637f75e65f1426d19a4d3
Ryan Pfister <57775823+ryanpfister@users.noreply.github.com>
2023-09-14 14:13:31 -0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -12,17 +12,18 @@ router.get("/", (req, res) => {
       <!DOCTYPE html>
       <html>
       <center>
-      <div id="adobe-dc-view" style="height: 360px; width: 500px;"></div>
+      <div id="adobe-dc-view" style="width: 800px;"></div>
       <script src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
       <script type="text/javascript">
           document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
               var adobeDCView = new AdobeDC.View({clientId: "927f8ab99a994083a4e0f931b8c1afc2", divId: "adobe-dc-view"});
               adobeDCView.previewFile({
-                  content:{location: {url: "https://wedding.rdpfister.com/uploads/Parking-Instructions.pdf"}},
-                  metaData:{fileName: "Parking-Instructions.pdf"}
-              }, {embedMode: "SIZED_CONTAINER"});
+                content:{location: {url: "https://wedding.rdpfister.com/uploads/Parking-Instructions.pdf"}},
+                metaData:{fileName: "Parking-Instructions.pdf"}
+            }, {embedMode: "IN_LINE"});
           });
       </script>
+
       </center>
      </html>
     `);
